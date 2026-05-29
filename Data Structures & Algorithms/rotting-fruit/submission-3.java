138432e639b228bb17768a023391e1c91936feb5
AstroBoy73 <42893328+balajianbalagan@users.noreply.github.com>
2026-05-29 23:09:18 +0530

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.java
@@ -0,0 +1,78 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int minutes = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<ArrayList<Integer>> q = new LinkedList<>();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==2){
+                   
+                    q.add(new ArrayList<>(List.of(i,j)));
+                }
+            }
+        }
+
+        while(!q.isEmpty()){
+            int s = q.size();
+            
+            int changed = 0;
+            for(int k=0;k<s;k++){
+                ArrayList<Integer> pos = q.poll();
+            int i = pos.get(0);
+            int j = pos.get(1);
+            System.out.println(i+" "+j);
+            if(i-1>=0){
+                if(grid[i-1][j]==1){
+                    
+                    grid[i-1][j] = 2;
+                    // q.add({i-1,j});
+                    q.add(new ArrayList<>(List.of(i-1,j)));
+                    changed++;
+                }
+            }
+            if(j-1>=0){
+                if(grid[i][j-1]==1){
+                    grid[i][j-1] = 2;
+                    // q.add({i,j-1});
+                    q.add(new ArrayList<>(List.of(i,j-1)));
+                    changed++;
+                }
+            }
+            if(i+1<grid.length){
+                if(grid[i+1][j]==1){
+                    grid[i+1][j] = 2;
+                    // q.add({i+1,j});
+                    q.add(new ArrayList<>(List.of(i+1,j)));
+                    changed++;
+                }
+            }
+            if(j+1<grid[0].length){
+                if(grid[i][j+1]==1){
+                    grid[i][j+1] = 2;
+                    // q.add({i,j+1});
+                    q.add(new ArrayList<>(List.of(i,j+1)));
+                    changed++;
+                }
+            }
+            
+            }
+           if(changed>0){
+             minutes++;
+           }
+            
+        }
+
+
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+
+        return minutes;
+    }
+}
